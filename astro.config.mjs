b52e54655753bfd95877d8363fc92683518d25c4
hokmts <h.1966.work@gmail.com>
2023-04-26 22:25:06 +0900

    001

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
@@ -7,4 +8,5 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://hokmts23-blog2023.netlify.app/',
 	integrations: [mdx(), sitemap()],
+	integrations: [partytown()],
 });
