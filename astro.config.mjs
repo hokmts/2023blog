d0e47fa9ccf7fe2b145b867b74cd3bc0fce2f0df
hokmts <h.1966.work@gmail.com>
2023-04-26 16:16:03 +0900

    001

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://hokmts23-blog2023.netlify.app/',
 	integrations: [mdx(), sitemap()],
 });
